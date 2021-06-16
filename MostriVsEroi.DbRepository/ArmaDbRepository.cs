35bfde109b87aeeea708125cbbf937cac74270da
francesca di cugno <fradicugno@gmail.com>
2021-06-16 15:28:00 +0200

    4 commit

---
+++ MostriVsEroi.DbRepository/ArmaDbRepository.cs
@@ -0,0 +1,42 @@
+﻿using MostriVsEroi.Modelli;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostriVsEroi.DbRepository
+{
+    class ArmaDbRepository : DbManager
+    {
+        public List<Arma> FetchArma(Utente utente)
+        {
+            List<Arma> armi = new List<Arma>();
+            //Console.WriteLine("Inserire il nome dell'arma cercata");
+            //string arma = Console.ReadLine();
+            Connessione(out SqlConnection connection, out SqlCommand cmd);
+            cmd.CommandText = "SELECT * from dbo.Armi ";
+            
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (!reader.HasRows)
+            {
+                Console.WriteLine("Arma non trovata");
+            }
+            else
+                while (reader.Read())
+                {
+                    var id = reader[0];
+                    var nome = reader[1];
+                    var puntiDanno = reader[2];
+                    Console.WriteLine($"{id} - {nome} - {puntiDanno}");
+                  
+                }
+            return armi;
+            
+        }
+
+
+
+    }
+}
---
+++ MostriVsEroi.DbRepository/EroeDbRepository.cs
@@ -0,0 +1,21 @@
+﻿using MostriVsEroi.Modelli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostriVsEroi.DbRepository
+{
+    class EroeDbRepository:DbManager
+    {
+        public List<Eroe> FetchEroi(Utente utente)
+        {
+            List<Eroe> eroi = new List<Eroe>();
+            //eroi.Add(new Eroe("PrimoEroeDiProva", new Arma("Ascia", 8), 1, CategoriaEroe.Guerriero, 20, 0));
+            //eroi.Add(new Eroe("SecondoEroeDiProva", new Arma("Bastone Magico", 10), 2, CategoriaEroe.Mago, 40, 0));
+
+            return eroi;
+        }
+    }
+}
